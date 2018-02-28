6a1e24b30eb8dc0811ff70532dd5b39c9b2c0673
Diego Lima Aparecido <diego.lima.aparecido@outlook.com>
2018-02-28 19:48:56 -0300

    Usando operações monetarias

--- Java Brasil Formate datas, cpf e números nacionais/source/src/main/java/br/com/alura/dinheiro/Dinheiro.java
+++ Java Brasil Formate datas, cpf e números nacionais/source/src/main/java/br/com/alura/dinheiro/Dinheiro.java
@@ -3,8 +3,13 @@ package br.com.alura.dinheiro;
 import javax.money.CurrencyUnit;
 import javax.money.Monetary;
 import javax.money.MonetaryAmount;
+import javax.money.NumberValue;
 
 import org.javamoney.moneta.Money;
+import org.javamoney.moneta.function.MonetaryOperators;
+
+import br.com.caelum.stella.inwords.FormatoDeReal;
+import br.com.caelum.stella.inwords.NumericToWordsConverter;
 
 public class Dinheiro {
 	public static void main(String[] args) {
@@ -13,5 +18,15 @@ public class Dinheiro {
 		System.out.println("VALOR DA PARCELA: " + valorDaParcela);
 		MonetaryAmount valorTotal = valorDaParcela.multiply(12);
 		System.out.println("VALOR TOTAL: " + valorTotal);
+
+		MonetaryAmount desconto = valorTotal.with(MonetaryOperators.percent(10));
+		System.out.println(desconto);
+
+		NumberValue descontoSemMoeda = desconto.getNumber();
+
+		NumericToWordsConverter conversor = new NumericToWordsConverter(new FormatoDeReal());
+		String valorPorExtenso = conversor.toWords(descontoSemMoeda.doubleValue());
+		System.out.println(valorPorExtenso);
+		System.out.println("Olá, Aluno. Ganhe " + valorPorExtenso + " ouvindo nosso podcast. LINK");
 	}
 }
