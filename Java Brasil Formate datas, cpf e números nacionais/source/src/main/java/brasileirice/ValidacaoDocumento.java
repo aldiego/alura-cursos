bec1eb05cf715e67de6f75610e56fd323d6b814f
Diego Lima Aparecido <diego.lima.aparecido@outlook.com>
2018-02-28 18:30:22 -0300

    Validacao CNPJ e Titulo
    Adicionado testes para validacao de CNPJ e titulo de eleitor.

--- Java Brasil Formate datas, cpf e números nacionais/source/src/main/java/brasileirice/ValidacaoDocumento.java
+++ Java Brasil Formate datas, cpf e números nacionais/source/src/main/java/brasileirice/ValidacaoDocumento.java
@@ -1,18 +1,54 @@
 package brasileirice;
 
+import br.com.caelum.stella.validation.CNPJValidator;
 import br.com.caelum.stella.validation.CPFValidator;
 import br.com.caelum.stella.validation.InvalidStateException;
+import br.com.caelum.stella.validation.TituloEleitoralValidator;
+import br.com.caelum.stella.validation.Validator;
 
 public class ValidacaoDocumento {
 	public static void main(String[] args) {
-		String cpf = "86288366757";
-		CPFValidator validador = new CPFValidator();
+		validaCPF();
+
+		validaCNPJ();
+
+		validaTituloEleitor();
+	}
+
+	private static void validaTituloEleitor() {
+		String tituloEleitor = "417453530116";
 		try {
-			validador.assertValid(cpf);
+			validarDocumentos(new TituloEleitoralValidator(), tituloEleitor);
+			System.out.println("TÍTULO VÁLIDO");
+
+		} catch (InvalidStateException e) {
+			System.out.println("TÍTULO INVÁLIDO :" + e);
+		}
+	}
+
+	private static void validaCNPJ() {
+		String cnpj = "82588641000173";
+		try {
+			validarDocumentos(new CNPJValidator(), cnpj);
+			System.out.println("CNPJ VÁLIDO");
+
+		} catch (InvalidStateException e) {
+			System.out.println("CNPJ INVÁLIDO :" + e);
+		}
+	}
+
+	private static void validaCPF() {
+		String cpf = "86288366757";
+		try {
+			validarDocumentos(new CPFValidator(), cpf);
 			System.out.println("CPF VÁLIDO");
 
 		} catch (InvalidStateException e) {
 			System.out.println("CPF INVÁLIDO : " + e);
 		}
+	}
+
+	private static void validarDocumentos(Validator<String> validador, String documento) {
+		validador.assertValid(documento);
 	}
 }
